f6bb9af50c71a52da90313ac3d50db8658cff407
Tarek592 <muhammadtarek5920@gmail.com>
2025-05-21 18:36:31 +0600

    update in new plants section

--- src/components/NewPlants.jsx
+++ src/components/NewPlants.jsx
@@ -21,7 +21,7 @@ const NewPlants = ({lastPlantData}) => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {lastPlantData.map((plant) => (
           <div
-            key={plant.id}
+            key={plant._id}
             className="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transition"
           >
             <img
